63c092ec6fb4533ddb581c0937bf6021b89782a2
Yeampier2022 <yeampierjesusa@gmail.com>
2023-09-01 10:45:52 -0400

    feat: handling overflow

--- lib/offers_price.dart
+++ lib/offers_price.dart
@@ -5,8 +5,22 @@ class OffersPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Offer(
-        title: "My great offer over", description: "Buy 1, get 10 for free");
+    return ListView(
+      children: const [
+        Offer(
+            title: "My great offer over",
+            description: "Buy 1, get 10 for free"),
+        Offer(
+            title: "My great offer over",
+            description: "Buy 1, get 10 for free"),
+        Offer(
+            title: "My great offer over",
+            description: "Buy 1, get 10 for free"),
+        Offer(
+            title: "My great offer over",
+            description: "Buy 1, get 10 for free"),
+      ],
+    );
   }
 }
 
@@ -22,21 +36,55 @@ class Offer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
+    return SizedBox(
+      height: 200,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Center(
-              child: Text(title,
-                  style: Theme.of(context).textTheme.headlineLarge)),
+          child: Card(
+            color: Colors.amber.shade50,
+            elevation: 7,
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(
+                      "assets/background.png",
+                    )),
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                        child: Container(
+                      color: Colors.amber.shade50,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(title,
+                            style: Theme.of(context).textTheme.headlineLarge),
+                      ),
+                    )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                        child: Container(
+                      color: Colors.amber.shade50,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(description,
+                            style: Theme.of(context).textTheme.headlineMedium),
+                      ),
+                    )),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Center(
-              child: Text(description,
-                  style: Theme.of(context).textTheme.headlineMedium)),
-        ),
-      ],
+      ),
     );
   }
 }
