8e7187a81068038e25dad5694fe8c0a189b5f66d
Yeampier2022 <yeampierjesusa@gmail.com>
2023-09-04 16:32:34 -0400

    feat: menuPage

--- lib/pages/menupage.dart
+++ lib/pages/menupage.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
 
+import '../datemodal.dart';
+
 class MenuPage extends StatelessWidget {
-  const MenuPage({super.key});
+  const MenuPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var p = Product(id: 1, name: "Product 1", price: 10, image: "product1.jpg");
+    return ProductItem(product: p);
+  }
+}
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  const ProductItem({Key? key, required this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        elevation: 4,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset("assets/black_coffee.png"),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                product.name,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(product.price.toString()),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
